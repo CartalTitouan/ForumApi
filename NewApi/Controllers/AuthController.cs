334d8a7151da0b6448fb6f30460d790170d1f5f4
Titouan Cartal <titouancartalgmail.com>
2026-04-23 17:12:35 +0200

    Maj de confort

--- NewApi/Controllers/AuthController.cs
+++ NewApi/Controllers/AuthController.cs
@@ -66,7 +66,7 @@ public class AuthController : ControllerBase
             issuer: _config["Jwt:Issuer"],
             audience: _config["Jwt:Audience"],
             claims: claims,
-            expires: DateTime.UtcNow.AddDays(7),
+            expires: DateTime.UtcNow.AddHours(4),
             signingCredentials: creds
         );
 
